fa2562174655f7166765dc9cb7a67a80c166c06b
MuhammadRaihanAlghifari <alghifari2096@gmail.com>
2025-09-01 16:29:45 +0700

    sisaan

--- routes/auth.js
+++ routes/auth.js
@@ -24,13 +24,3 @@ router.post("/register", authController.register);
 
 module.exports = router;
 
-/*
-router.get('/login', function(req, res, next) {
-    res.render('auth/login', { 
-      title: 'Login',
-      layout: 'layouts/layout_login', 
-    });
-});
-  
-router.post('/login', login);
-*/
